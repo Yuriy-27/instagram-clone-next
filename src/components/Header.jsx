a14270c7346fc87c6d207771f7b3d23b025679fc
Yuriy Shchebetun <yuriyshchebetun@gmail.com>
2024-05-29 12:36:49 +0300

    add modal functionality using react-modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
 "use client";
 
+import { useRef, useState } from "react";
 import { signIn, useSession, signOut } from "next-auth/react";
-// import { IoMdAddCircleOutline } from "react-icons/io";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { HiCamera } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 import Image from "next/image";
 import Link from "next/link";
+import Modal from "react-modal";
 
 export default function Header() {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [postUploading, setPostUploading] = useState(false);
+  const [caption, setCaption] = useState("");
+  const filePickerRef = useRef(null);
+
   return (
     <div className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
       <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -40,10 +52,10 @@ export default function Header() {
 
         {session ? (
           <div className='flex gap-2 items-center'>
-            {/* <IoMdAddCircleOutline
+            <IoMdAddCircleOutline
               className='text-2xl cursor-pointer tranform hover:scale-125 transition duration-300 hover:text-red-600'
               onClick={() => setIsOpen(true)}
-            /> */}
+            />
             <img
               src={session.user.image}
               alt={session.user.name}
@@ -60,6 +72,62 @@ export default function Header() {
           </button>
         )}
       </div>
+      {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          className='max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md'
+          onRequestClose={() => setIsOpen(false)}
+          ariaHideApp={false}
+        >
+          <div className='flex flex-col justify-center items-center h-[100%]'>
+            {selectedFile ? (
+              <img
+                onClick={() => setSelectedFile(null)}
+                src={imageFileUrl}
+                alt='selected file'
+                className={`w-full max-h-[250px] object-over cursor-pointer ${
+                  imageFileUploading ? "animate-pulse" : ""
+                }`}
+              />
+            ) : (
+              <HiCamera
+                onClick={() => filePickerRef.current.click()}
+                className='text-5xl text-gray-400 cursor-pointer'
+              />
+            )}
+            <input
+              hidden
+              ref={filePickerRef}
+              type='file'
+              accept='image/*'
+              // onChange={addImageToPost}
+            />
+          </div>
+          <input
+            type='text'
+            maxLength='150'
+            placeholder='Please enter you caption...'
+            className='m-4 border-none text-center w-full focus:ring-0 outline-none'
+            onChange={(e) => setCaption(e.target.value)}
+          />
+          <button
+            // onClick={handleSubmit}
+            disabled={
+              !selectedFile ||
+              caption.trim() === "" ||
+              postUploading ||
+              imageFileUploading
+            }
+            className='w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100'
+          >
+            Upload Post
+          </button>
+          <AiOutlineClose
+            className='cursor-pointer absolute top-2 right-2 hover:text-red-600 transition duration-300'
+            onClick={() => setIsOpen(false)}
+          />
+        </Modal>
+      )}
     </div>
   );
 }
